c71a85799ff143d76ec667b8c7f6dfa696027b56
yana-srfn <srfn.dayana@gmail.com>
2026-07-28 19:49:32 +0800

    inspection routes js

--- routes/inspectionRoutes.js
+++ routes/inspectionRoutes.js
@@ -1,1 +1,14 @@
-// dashboard model done by dayana
+// inspection routes done by dayana
+const express = require("express");
+const router = express.Router();
+
+const inspectionController =
+    require("../controllers/inspectionController");
+
+// create a completed inspection
+router.post(
+    "/",
+    inspectionController.createInspection
+);
+
+module.exports = router;
