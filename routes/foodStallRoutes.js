1812c090dad6dadbc6a92730a0ff2dca4821420e
yana-srfn <srfn.dayana@gmail.com>
2026-07-28 17:14:30 +0800

    get nea-search for foodstall

--- routes/foodStallRoutes.js
+++ routes/foodStallRoutes.js
@@ -4,6 +4,12 @@ const router = express.Router();
 const foodStallController =
     require("../controllers/foodStallController");
 
+    // get all food stalls with their latest inspection details for nea search
+router.get(
+    "/nea-search",
+    foodStallController.getFoodStallsForNEASearch
+);
+
 router.get("/hawker-centre/:hawkerCentreID",foodStallController.getStallsByHawkerCentre);
 router.get("/:stallID",foodStallController.getFoodStallById);
 
